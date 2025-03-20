9b7099e537cc7130a97b382d79fc8e70457617a1
Viktoriia Khripko <vic.khripko@gmail.com>
2025-03-20 11:09:43 +0200

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { CategoryPageComponent } from './pages/category-page/category-page.component';
-
-export const routes: Routes = [
-    { path: '', component: HomePageComponent },
-    { path: 'category', component: CategoryPageComponent }
-];
